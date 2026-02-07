0f73abfe32ff44ea8b1a14b10d7d8cfccf6ff8b4
256bitguy <ashishrajsri2233@gmail.com>
2026-02-08 01:51:46 +0530

    firebase development

--- config/firebase.js
+++ config/firebase.js
@@ -1,15 +1,30 @@
 // /config/firebase-admin.js
+// import admin from "firebase-admin";
+
+// console.log("🔥 Firebase Admin apps before init:", admin.apps.length);
+
+// if (!admin.apps.length) {
+//   admin.initializeApp({
+//     credential: admin.credential.applicationDefault(),
+//   });
+//   console.log("✅ Firebase Admin initialized");
+// } else {
+//   console.log("✅ Firebase Admin already initialized");
+// }
+
+// export default admin;
 import admin from "firebase-admin";
 
-console.log("🔥 Firebase Admin apps before init:", admin.apps.length);
-
 if (!admin.apps.length) {
+  const serviceAccount = JSON.parse(
+    process.env.FIREBASE_SERVICE_ACCOUNT
+  );
+
   admin.initializeApp({
-    credential: admin.credential.applicationDefault(),
+    credential: admin.credential.cert(serviceAccount),
   });
+
   console.log("✅ Firebase Admin initialized");
-} else {
-  console.log("✅ Firebase Admin already initialized");
 }
 
 export default admin;
